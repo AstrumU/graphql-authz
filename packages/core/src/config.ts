a5282780b23625008a04217c0bf987a2e14a61a1
Kerim <kerim.charfi@ionix.cc>
2022-08-02 18:54:26 +0200

    responding to comments

--- packages/core/src/config.ts
+++ packages/core/src/config.ts
@@ -1,6 +1,7 @@
 import { AuthSchema } from './auth-schema';
 import { RulesObject } from './rules';
 import { processError as defaultProcessError } from './process-error';
+import { IAuthConfig } from './auth-config';
 
 export interface IAuthZConfig {
   rules: RulesObject;
@@ -14,44 +15,48 @@ export function completeConfig(
   config: IAuthZConfig
 ): Omit<Required<IAuthZConfig>, 'authSchema'> & { authSchema?: AuthSchema } {
 
-  if (config.authSchema) {
-    // Check if rules in authSchema actually exist in rules definition
-    for (const typeName in config.authSchema) {
-      const typeRuleDefinitions = config.authSchema[typeName];
-      if ('__authz' in typeRuleDefinitions) {
-        // check type level rules
-        //@ts-ignore
-        const ruleNames = typeRuleDefinitions.__authz?.rules;
-        if(!ruleNames) {
-          console.warn('unexpected shape of authSchema for type ' + typeName);
-          continue
-        }
-        let ruleName: string
-        for (ruleName of ruleNames as string[]) {
-          if (!(ruleName in config.rules)) {
-            throw new Error(`Rule ${ruleName} not found in rules`);
-          }
-        }
-      } else {
-        // check field level rules
-        for (const fieldName in typeRuleDefinitions) {
-          //@ts-ignore
-          const ruleNames = typeRuleDefinitions[fieldName].__authz.rules;
-          let ruleName: string
-          for (ruleName of ruleNames) {
-            if (!(ruleName in config.rules)) {
-              throw new Error(`Rule ${ruleName} not found in rules`);
-            }
-          }
-        }
-      }
-    }
-  }
-
-  return {
+  const resultConfig = {
     directiveName: 'authz',
     authSchemaKey: '__authz',
     processError: defaultProcessError,
     ...config
   };
+
+  if (config.authSchema) {
+    // Check if rules in authSchema actually exist in rules definition
+    const authSchemaRules = [] as (IAuthConfig<RulesObject> | string[] | undefined)[];
+
+    // get all rules from authSchema into list
+    for (const typeName in config.authSchema) {
+      const typeRuleDefinitions = config.authSchema[typeName];
+      if (resultConfig.authSchemaKey in typeRuleDefinitions) {
+        // type level rules
+        authSchemaRules.push(typeRuleDefinitions[resultConfig.authSchemaKey]?.rules);
+      } else {
+        // field level rules
+        for (const fieldName in typeRuleDefinitions ) {
+          const fieldRuleDefinitions = typeRuleDefinitions[fieldName] as Record<string, IAuthConfig<RulesObject>>;
+          if (resultConfig.authSchemaKey in fieldRuleDefinitions) {
+             authSchemaRules.push(fieldRuleDefinitions[resultConfig.authSchemaKey]?.rules);
+          }
+        }
+      }
+    }
+
+    // check if all rules in authSchema actually exist in config.rules definition
+    for (const authSchemaRule of authSchemaRules) {
+      if (!authSchemaRule || !Array.isArray(authSchemaRule)) {
+        continue;
+      }
+      let ruleName: string;
+      for (ruleName of authSchemaRule) {
+        if (!(ruleName in config.rules)) {
+          throw new Error(`Rule ${ruleName} not found in rules`);
+        }
+      }
+    }
+
+  }
+
+  return resultConfig;
 }
